d95ee47e4188a76741a8cd6d64e730cf571b5201
promodnl <promod.kumar@nineleaps.com>
2020-07-19 11:07:39 +0530

    login|SignUp page | HomePage with apicall

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import Login from './Login';
+import SignUp from './SignUp';
+import Home from './Home'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/register" exact component={SignUp} />
+        <Route path="/Home" exact component={Home} />
+
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
